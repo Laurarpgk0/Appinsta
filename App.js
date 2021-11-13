f31fb5844fc01a5a23869c1a88780dc232df3b13
Laura Gomes <laurazt6@gmail.com>
2021-11-13 19:26:26 -0300

    App

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+import Header from "./src/Header";
+import List from "./src/List";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [feed, setFeed] = useState([
+        {
+            id: "1",
+            nome: "Laura Gomes",
+            descricao: "Mais um dia de muitos bugs :)",
+            imgperfil:
+                "https://sujeitoprogramador.com/instareact/fotoPerfil1.png",
+            imgPublicacao:
+                "https://sujeitoprogramador.com/instareact/foto1.png",
+            likeada: true,
+            likers: 1,
+        },
+        {
+            id: "2",
+            nome: "Laura Gomes",
+            descricao: "Isso sim é ser raiz!!!!!",
+            imgperfil:
+                "https://sujeitoprogramador.com/instareact/fotoPerfil2.png",
+            imgPublicacao:
+                "https://sujeitoprogramador.com/instareact/foto2.png",
+            likeada: false,
+            likers: 0,
+        },
+        {
+            id: "3",
+            nome: "Laura Gomes",
+            descricao:
+                "Bora trabalhar, hoje estou começando em um projeto novo aqui no sujeito, desde o backend ao frontend",
+            imgperfil:
+                "https://sujeitoprogramador.com/instareact/fotoPerfil3.png",
+            imgPublicacao:
+                "https://sujeitoprogramador.com/instareact/foto3.png",
+            likeada: false,
+            likers: 3,
+        },
+        {
+            id: "4",
+            nome: "Laura Gomes",
+            descricao: "Isso sim que é TI!",
+            imgperfil:
+                "https://sujeitoprogramador.com/instareact/fotoPerfil1.png",
+            imgPublicacao:
+                "https://sujeitoprogramador.com/instareact/foto4.png",
+            likeada: false,
+            likers: 1,
+        },
+        {
+            id: "5",
+            nome: "Laura Gomes",
+            descricao: "Boa tarde galera do insta...",
+            imgperfil:
+                "https://sujeitoprogramador.com/instareact/fotoPerfil2.png",
+            imgPublicacao:
+                "https://sujeitoprogramador.com/instareact/foto5.png",
+            likeada: false,
+            likers: 32,
+        },
+    ]);
+    return (
+        <View style={styles.container}>
+            <Header />
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                keyExtractor={(item) => item.id}
+                data={feed}
+                renderItem={({ item }) => <List data={item} />}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
 });
---
+++ src/Header/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+
+export default function Header() {
+    return (
+        <View style={styles.header}>
+            <TouchableOpacity>
+                <Image
+                    source={require("../img/logo.png")}
+                    style={styles.logo}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Image
+                    source={require("../img/send.png")}
+                    style={styles.send}
+                />
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    header: {
+        height: 80,
+        backgroundColor: "#fff",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderBottomWidth: 0.2,
+        shadowColor: "#000",
+        elevation: 2,
+    },
+    send: {
+        width: 23,
+        height: 23,
+    },
+});
